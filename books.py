2d4a9f247259bcc070c29d439f720bfabf16c151
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-25 13:14:17 -0300

    Created show_all_books() function

--- books.py
+++ books.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-def first_api():
-    return {'message': 'Hello, Pedro!'}
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'genre': 'History'},
+    {'title': 'Title Two', 'author': 'Author Two', 'genre': 'Fiction'},
+    {'title': 'Title Three', 'author': 'Author Three', 'genre': 'Sci-fi'},
+    {'title': 'Title Four', 'author': 'Author Four', 'genre': 'Fiction'},
+    {'title': 'Title Five', 'author': 'Author Two', 'genre': 'History'},
+]
+
+@app.get("/books")
+def show_all_books():
+    return BOOKS
