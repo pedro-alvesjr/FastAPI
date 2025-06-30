987f12ea5393885fdaa62f2cccf2f99d4a93e087
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-06-29 21:49:41 -0300

    books.py updated
    Added read_category_by_query

--- books.py
+++ books.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BOOKS = [
-    {'title': 'Title One', 'author': 'Author One', 'genre': 'History'},
-    {'title': 'Title Two', 'author': 'Author Two', 'genre': 'Fiction'},
-    {'title': 'Title Three', 'author': 'Author Three', 'genre': 'Sci-fi'},
-    {'title': 'Title Four', 'author': 'Author Four', 'genre': 'Fiction'},
-    {'title': 'Title Five', 'author': 'Author Two', 'genre': 'History'},
+    {'title': 'Title One', 'author': 'Author One', 'category': 'History'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'Fiction'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'Sci-fi'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'Fiction'},
+    {'title': 'Title Five', 'author': 'Author Two', 'category': 'History'},
 ]
 
 @app.get("/books")
@@ -19,3 +19,11 @@ def show_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+@app.get('/books/')
+def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
